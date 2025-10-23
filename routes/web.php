c651696a537c1ce4018f5a376cc62619a07caf3d
Sand School <trantruong7826@gmail.com>
2025-10-23 08:41:53 +0700

    chỉnh sửa

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +20,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+
+// login customer
+Route::get('/customer/login', [CustomerController::class, 'login'])->name('customer.login');
+// ... các route khác

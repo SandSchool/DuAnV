468e601148a99c5e039a7bcdedb6751901dba927
Sand School <trantruong7826@gmail.com>
2025-11-09 03:23:25 +0700

    có thay đổi

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+
 
 
 
@@ -20,11 +22,47 @@ use App\Http\Controllers\HomeController;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-
+// Trang chu
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
-
-
+Route::get('/about', [HomeController::class, 'about'])->name('home.about');
 // login customer
-Route::get('/customer/login', [CustomerController::class, 'login'])->name('customer.login');
-Route::get('/customer/register', [CustomerController::class, 'register'])->name('customer.register');
-// ... các route khác
+Route::prefix('customer')->group(function () {
+
+    // Routes cho Guest (chưa đăng nhập) - SỬA THÀNH 'guest:cus'
+    Route::middleware('guest:cus')->group(function () {
+        // Login
+        Route::get('login', [CustomerController::class, 'showLoginForm'])
+            ->name('customer.login');
+        Route::post('login', [CustomerController::class, 'handleLogin']);
+
+        // Register
+        Route::get('register', [CustomerController::class, 'showRegisterForm'])
+            ->name('customer.register');
+        Route::post('register', [CustomerController::class, 'handleRegister']);
+    });
+
+    // Routes cho User đã đăng nhập - SỬA THÀNH 'auth:cus'
+    Route::middleware('auth:cus')->group(function () {
+        // Logout
+        Route::post('logout', [CustomerController::class, 'handleLogout'])
+            ->name('customer.logout');
+
+        // Profile
+        Route::get('profile', [CustomerController::class, 'showProfile'])
+            ->name('customer.profile');
+
+        // XÓA ROUTE POST BỊ TRÙNG
+        // Route::post('profile', [CustomerController::class, 'showProfile'])
+        //     ->name('customer.profile'); 
+
+        Route::post('profile/update', [CustomerController::class, 'updateProfile'])
+            ->name('customer.profile.update');
+
+        // Order history
+        Route::get('orders', [CustomerController::class, 'showOrders'])
+            ->name('customer.orders');
+    });
+});
+// product routes
+Route::get('/product/index', [ProductController::class, 'index'])->name('product.index');
+Route::get('/product/detail/{product}', [ProductController::class, 'detail'])->name('product.detail');

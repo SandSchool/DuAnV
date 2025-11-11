eb4827a514d773e4475814c9120d26b2424e98b1
Sand School <trantruong7826@gmail.com>
2025-11-11 10:44:40 +0700

    cập nhật hiển thị Product

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,203 +1,118 @@
 @extends("layout.default")
-@section("main")
-<!-- Title Page -->
-<section class="bg-title-page p-t-50 p-b-40 flex-col-c-m" style="background-image: url({{ asset('public/images') }}/heading-pages-02.jpg);">
-    <h2 class="l-text2 t-center">
-        Women
-    </h2>
-    <p class="m-text13 t-center">
-        New Arrivals Women Collection 2018
-    </p>
-</section>
+@section("content")
 
 
-<!-- Content page -->
-<section class="bgwhite p-t-55 p-b-65">
+<!-- Page Header -->
+<div class="product-page-header">
+    <div class="container">
+        <h1><i class="fas fa-shoe-prints me-3"></i>Bộ Sưu Tập Giày</h1>
+        <p>Khám phá những mẫu giày thể thao và thời trang mới nhất 2024</p>
+    </div>
+</div>
+
+<!-- Content -->
+<section class="py-5">
     <div class="container">
         <div class="row">
-            <div class="col-sm-6 col-md-4 col-lg-3 p-b-50">
-                <div class="leftbar p-r-20 p-r-0-sm">
-                    <!--  -->
-                    <h4 class="m-text14 p-b-7">
-                        Categories
+            <!-- Sidebar Filter -->
+            <div class="col-lg-3 col-md-4">
+                <div class="filter-sidebar">
+                    <h4 class="filter-title">
+                        <i class="fas fa-filter me-2"></i>Danh Mục
                     </h4>
-
-                    <ul class="p-b-54">
-                        <li class="p-t-4">
-                            <a href="{{ url("product") }}" class="s-text13 active1">
-                                All
+                    <ul class="category-list">
+                        <li>
+                            <a href="{{ url('product') }}" class="{{ !request('category') ? 'active' : '' }}">
+                                <i class="fas fa-th me-2"></i>Tất cả sản phẩm
                             </a>
                         </li>
-
                         @foreach($category_data as $category)
-                        <li class="p-t-4">
-                            <a href="{{ url("product") }}?category={{ $category->MALOAI }}" class="s-text13">
-                                {{ $category->TENLOAI }}
+                        <li>
+                            <a href="{{ url('product') }}?category={{ $category->MALOAI }}"
+                                class="{{ request('category') == $category->MALOAI ? 'active' : '' }}">
+                                <i class="fas fa-chevron-right me-2"></i>{{ $category->TENLOAI }}
                             </a>
                         </li>
                         @endforeach
-
                     </ul>
 
-                    <!--  -->
-                    <h4 class="m-text14 p-b-32">
-                        Filters
-                    </h4>
-
-                    <div class="filter-price p-t-22 p-b-50 bo3">
-                        <div class="m-text15 p-b-17">
-                            Price
-                        </div>
-
-                        <div class="wra-filter-bar">
-                            <div id="filter-bar"></div>
-                        </div>
-
-                        <div class="flex-sb-m flex-w p-t-16">
-                            <div class="w-size11">
-                                <!-- Button -->
-                                <button class="flex-c-m size4 bg7 bo-rad-15 hov1 s-text14 trans-0-4">
-                                    Filter
-                                </button>
-                            </div>
-
-                            <div class="s-text3 p-t-10 p-b-10">
-                                Range: $<span id="value-lower">610</span> - $<span id="value-upper">980</span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="filter-color p-t-22 p-b-50 bo3">
-                        <div class="m-text15 p-b-12">
-                            Color
-                        </div>
-
-                        <ul class="flex-w">
-                            <li class="m-r-10">
-                                <input class="checkbox-color-filter" id="color-filter1" type="checkbox" name="color-filter1">
-                                <label class="color-filter color-filter1" for="color-filter1"></label>
-                            </li>
-
-                            <li class="m-r-10">
-                                <input class="checkbox-color-filter" id="color-filter2" type="checkbox" name="color-filter2">
-                                <label class="color-filter color-filter2" for="color-filter2"></label>
-                            </li>
-
-                            <li class="m-r-10">
-                                <input class="checkbox-color-filter" id="color-filter3" type="checkbox" name="color-filter3">
-                                <label class="color-filter color-filter3" for="color-filter3"></label>
-                            </li>
-
-                            <li class="m-r-10">
-                                <input class="checkbox-color-filter" id="color-filter4" type="checkbox" name="color-filter4">
-                                <label class="color-filter color-filter4" for="color-filter4"></label>
-                            </li>
-
-                            <li class="m-r-10">
-                                <input class="checkbox-color-filter" id="color-filter5" type="checkbox" name="color-filter5">
-                                <label class="color-filter color-filter5" for="color-filter5"></label>
-                            </li>
-
-                            <li class="m-r-10">
-                                <input class="checkbox-color-filter" id="color-filter6" type="checkbox" name="color-filter6">
-                                <label class="color-filter color-filter6" for="color-filter6"></label>
-                            </li>
-
-                            <li class="m-r-10">
-                                <input class="checkbox-color-filter" id="color-filter7" type="checkbox" name="color-filter7">
-                                <label class="color-filter color-filter7" for="color-filter7"></label>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div class="search-product pos-relative bo4 of-hidden">
-                        <input class="s-text7 size6 p-l-23 p-r-50" type="text" name="search-product" placeholder="Search Products...">
-
-                        <button id="btnSearch" class="flex-c-m size5 ab-r-m color2 color0-hov trans-0-4">
-                            <i class="fs-12 fa fa-search" aria-hidden="true"></i>
-                        </button>
+                    <!-- Search Box -->
+                    <div class="search-box">
+                        <form action="{{ url('product') }}" method="GET">
+                            <input type="text" name="search" placeholder="Tìm kiếm sản phẩm..."
+                                value="{{ request('search') }}">
+                            <button type="submit">
+                                <i class="fas fa-search"></i>
+                            </button>
+                        </form>
                     </div>
                 </div>
             </div>
 
-            <div class="col-sm-6 col-md-8 col-lg-9 p-b-50">
-                <!--  -->
-                <div class="flex-sb-m flex-w p-b-35">
-                    <div class="flex-w">
-                        <div class="rs2-select2 bo4 of-hidden w-size12 m-t-5 m-b-5 m-r-10">
-                            <select class="selection-2" name="sorting">
-                                <option>Default Sorting</option>
-                                <option>Popularity</option>
-                                <option>Price: low to high</option>
-                                <option>Price: high to low</option>
-                            </select>
-                        </div>
-
-                        <div class="rs2-select2 bo4 of-hidden w-size12 m-t-5 m-b-5 m-r-10">
-                            <select class="selection-2" name="sorting">
-                                <option>Price</option>
-                                <option>$0.00 - $50.00</option>
-                                <option>$50.00 - $100.00</option>
-                                <option>$100.00 - $150.00</option>
-                                <option>$150.00 - $200.00</option>
-                                <option>$200.00+</option>
-
-                            </select>
-                        </div>
-                    </div>
-
-                    <span class="s-text8 p-t-5 p-b-5">
-                        Showing 1–12 of 16 results
+            <!-- Products Grid -->
+            <div class="col-lg-9 col-md-8">
+                <!-- Results Info -->
+                <div class="results-info">
+                    <span style="color: #666; font-weight: 500;">
+                        <i class="fas fa-box me-2"></i>
+                        Hiển thị {{ $product_data->firstItem() ?? 0 }} – {{ $product_data->lastItem() ?? 0 }}
+                        của {{ $product_data->total() }} sản phẩm
                     </span>
+                    <div>
+                        <select class="sort-select">
+                            <option>Mới nhất</option>
+                            <option>Giá: Thấp đến cao</option>
+                            <option>Giá: Cao đến thấp</option>
+                            <option>Phổ biến nhất</option>
+                        </select>
+                    </div>
                 </div>
 
-                <!-- Product -->
-                <div class="row">
-                    @foreach($product_data as $product)
-                    <div class="col-sm-12 col-md-6 col-lg-4 p-b-50">
-                        <!-- Block2 -->
-                        <div class="block2">
-                            <div class="block2-img wrap-pic-w of-hidden pos-relative block2-labelnew">
-                                <img src="{{ $product->HINHANH }}" alt="IMG-PRODUCT">
-
-                                <div class="block2-overlay trans-0-4">
-                                    <a href="#" class="block2-btn-addwishlist hov-pointer trans-0-4">
-                                        <i class="icon-wishlist icon_heart_alt" aria-hidden="true"></i>
-                                        <i class="icon-wishlist icon_heart dis-none" aria-hidden="true"></i>
-                                    </a>
-
-                                    <div class="block2-btn-addcart w-size1 trans-0-4">
-                                        <!-- Button -->
-                                        <a href="{{ url('cart/add') }}/{{ $product->MASP }}" class="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
-                                            Add to Cart
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="block2-txt p-t-20">
-                                <a href="{{ route("product.detail", ["product" => $product->MASP]) }}" class="block2-name dis-block s-text3 p-b-5">
-                                    {{ $product->TENSP }}
+                <!-- Product Grid -->
+                <div class="product-grid">
+                    @forelse($product_data as $product)
+                    <div class="product-card">
+                        <div class="product-image">
+                            <img src="{{ $product->HINHANH }}" alt="{{ $product->TENSP }}">
+                            <span class="product-badge">New</span>
+                            <div class="product-actions">
+                                <button class="action-btn" title="Yêu thích">
+                                    <i class="fas fa-heart"></i>
+                                </button>
+                                <a href="{{ route('product.detail', ['product' => $product->MASP]) }}"
+                                    class="action-btn" title="Xem chi tiết">
+                                    <i class="fas fa-eye"></i>
                                 </a>
-
-                                <span class="block2-price m-text6 p-r-5">
-                                    {{ number_format($product->GIA) }} &#8363;
-                                </span>
                             </div>
                         </div>
+                        <div class="product-info">
+                            <a href="{{ route('product.detail', ['product' => $product->MASP]) }}"
+                                class="product-name">
+                                {{ $product->TENSP }}
+                            </a>
+                            <div class="product-price">
+                                {{ number_format($product->GIA, 0, ',', '.') }}đ
+                            </div>
+                            <a href="{{ url('cart/add') }}/{{ $product->MASP }}"
+                                class="add-to-cart-btn">
+                                <i class="fas fa-shopping-cart me-2"></i>Thêm vào giỏ
+                            </a>
+                        </div>
                     </div>
-                    @endforeach
+                    @empty
+                    <div class="col-12 text-center py-5">
+                        <i class="fas fa-box-open" style="font-size: 80px; color: #ddd;"></i>
+                        <h3 class="mt-3" style="color: #999;">Không tìm thấy sản phẩm nào</h3>
+                    </div>
+                    @endforelse
                 </div>
 
                 <!-- Pagination -->
-                <div class="pagination flex-m flex-w p-t-26">
-                    {{ $product_data->links("product.pagination") }}
+                @if($product_data->hasPages())
+                <div class="d-flex justify-content-center mt-5">
+                    {{ $product_data->links('product.pagination') }}
                 </div>
-
-                {{-- <div class="pagination flex-m flex-w p-t-26">--}}
-                {{-- <a href="#" class="item-pagination flex-c-m trans-0-4 active-pagination">1</a>--}}
-                {{-- <a href="#" class="item-pagination flex-c-m trans-0-4">2</a>--}}
-                {{-- </div>--}}
+                @endif
             </div>
         </div>
     </div>
--- resources/views/product/pagination.blade.php
+++ resources/views/product/pagination.blade.php
@@ -1,14 +1,128 @@
-<!-- kiem tra xem co du items de phan trang khong -->
-<!-- tra ve true neu co nhieu trang -->
-<!-- tra ve false neu tat ca cac item nam tren 1 trang don -->
+<style>
+    .modern-pagination {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 10px;
+        flex-wrap: wrap;
+        padding: 30px 0;
+    }
+
+    .pagination-item {
+        min-width: 45px;
+        height: 45px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 10px;
+        text-decoration: none;
+        font-weight: 600;
+        font-size: 15px;
+        transition: all 0.3s ease;
+        border: 2px solid #e0e0e0;
+        background: white;
+        color: #666;
+    }
+
+    .pagination-item:hover {
+        border-color: #667eea;
+        background: linear-gradient(135deg, rgba(102, 126, 234, 0.1) 0%, rgba(118, 75, 162, 0.1) 100%);
+        color: #667eea;
+        transform: translateY(-2px);
+        box-shadow: 0 5px 15px rgba(102, 126, 234, 0.2);
+    }
+
+    .pagination-item.active {
+        background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
+        color: white;
+        border-color: transparent;
+        box-shadow: 0 5px 20px rgba(102, 126, 234, 0.4);
+    }
+
+    .pagination-item.disabled {
+        opacity: 0.5;
+        cursor: not-allowed;
+        pointer-events: none;
+    }
+
+    .pagination-nav {
+        width: 45px;
+        height: 45px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 10px;
+        text-decoration: none;
+        font-size: 16px;
+        transition: all 0.3s ease;
+        border: 2px solid #667eea;
+        background: white;
+        color: #667eea;
+    }
+
+    .pagination-nav:hover:not(.disabled) {
+        background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
+        color: white;
+        transform: translateY(-2px);
+        box-shadow: 0 5px 15px rgba(102, 126, 234, 0.3);
+    }
+
+    .pagination-dots {
+        color: #999;
+        font-weight: 600;
+        padding: 0 10px;
+        font-size: 18px;
+    }
+</style>
+
 @if($paginator->hasPages())
-@foreach($elements as $element) <!-- duyet qua tung trang -->
-@foreach($element as $page => $url) <!-- duyet qua trang hien tai -->
-<a href="{{ $url }}" class="
-                item-pagination flex-c-m trans-0-4
-                @if($page == $paginator->currentPage()) active-pagination @endif">
-    {{ $page }}
-</a>
-@endforeach
-@endforeach
+<div class="modern-pagination">
+    {{-- Previous Button --}}
+    @if($paginator->onFirstPage())
+    <span class="pagination-nav disabled">
+        <i class="fas fa-chevron-left"></i>
+    </span>
+    @else
+    <a href="{{ $paginator->previousPageUrl() }}" class="pagination-nav" title="Trang trước">
+        <i class="fas fa-chevron-left"></i>
+    </a>
+    @endif
+
+    {{-- Pagination Elements --}}
+    @foreach($elements as $element)
+    {{-- "Three Dots" Separator --}}
+    @if(is_string($element))
+    <span class="pagination-dots">{{ $element }}</span>
+    @endif
+
+    {{-- Array Of Links --}}
+    @if(is_array($element))
+    @foreach($element as $page => $url)
+    @if($page == $paginator->currentPage())
+    <span class="pagination-item active">{{ $page }}</span>
+    @else
+    <a href="{{ $url }}" class="pagination-item">{{ $page }}</a>
+    @endif
+    @endforeach
+    @endif
+    @endforeach
+
+    {{-- Next Button --}}
+    @if($paginator->hasMorePages())
+    <a href="{{ $paginator->nextPageUrl() }}" class="pagination-nav" title="Trang sau">
+        <i class="fas fa-chevron-right"></i>
+    </a>
+    @else
+    <span class="pagination-nav disabled">
+        <i class="fas fa-chevron-right"></i>
+    </span>
+    @endif
+</div>
+
+{{-- Page Info --}}
+<div class="text-center" style="color: #999; font-size: 14px; margin-top: 15px;">
+    <i class="fas fa-info-circle me-2"></i>
+    Trang {{ $paginator->currentPage() }} / {{ $paginator->lastPage() }}
+    (Tổng {{ $paginator->total() }} sản phẩm)
+</div>
 @endif

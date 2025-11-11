098b8426fcbe0c9f499039033cfdaa72a9cd212f
Sand School <trantruong7826@gmail.com>
2025-11-11 10:54:43 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+// THÊM 2 DÒNG NÀY VÀO:
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Session; // Hoặc dùng helper session()
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // THÊM ĐOẠN CODE NÀY VÀO:
+        // Sử dụng View Composer để chia sẻ $cartCount với layout 'layout.default'
+        // Bạn có thể đổi 'layout.default' thành tên file layout chính của bạn
+        View::composer(['layout.default'], function ($view) {
+
+            // Lấy giỏ hàng từ session
+            $cart = Session::get('cart', []);
+
+            // Đếm số lượng sản phẩm *khác nhau* trong giỏ hàng
+            $cartCount = count($cart);
+
+            // Chia sẻ biến $cartCount với view
+            $view->with('cartCount', $cartCount);
+        });
     }
 }
